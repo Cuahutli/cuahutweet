f1cfa84dfd4ce3f7175dc4f5ad80767da4e39a63
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-16 12:58:05 -0700

    Implement my first views for CRUD operations in the app tweets.
    create a templates and views for detail an list tweets.

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Create
+
+# Update
+
+# Delete
+
+# List/Search
+
+# Retrieve
+from .models import Tweet
+
+
+def tweet_detail_view(request, id=1):
+    obj = Tweet.objects.get(id=id) #GET from database
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
+
+
+def tweet_list_view(request):
+    queryset = Tweet.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "tweets/list_view.html", context)
