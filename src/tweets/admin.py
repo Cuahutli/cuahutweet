48b7a2feb50a273714243ceda6a6bef92ce2d9d4
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-16 15:15:43 -0700

    How to customize Forms from Django admin and add Validation Error Messages.

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 
 from .models import Tweet
 
+from .form import TweetModelForm
 
-admin.site.register(Tweet)
+
+class TweetModelAdmin(admin.ModelAdmin):
+    form = TweetModelForm
+
+admin.site.register(Tweet, TweetModelAdmin)
