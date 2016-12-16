ecaeaa260b7f251b71bab2a458e3bad5e5e21046
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-16 13:23:05 -0700

    how to use Class Based Views

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView, ListView
+from .models import Tweet
 # Create your views here.
 
 # Create
@@ -11,20 +12,16 @@ from django.shortcuts import render
 # List/Search
 
 # Retrieve
-from .models import Tweet
 
 
-def tweet_detail_view(request, id=1):
-    obj = Tweet.objects.get(id=id) #GET from database
-    context = {
-        "object": obj
-    }
-    return render(request, "tweets/detail_view.html", context)
 
-
-def tweet_list_view(request):
+class TweetDetailView(DetailView):
+    template_name = 'tweets/detail_view.html'
     queryset = Tweet.objects.all()
-    context = {
-        "object_list": queryset
-    }
-    return render(request, "tweets/list_view.html", context)
+
+    def get_object(self):
+        return Tweet.objects.get(id=1)
+
+class TweetListView(ListView):
+    template_name = 'tweets/list_view.html'
+    queryset = Tweet.objects.all()
