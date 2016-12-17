0db5c97ae6345364a498bcdc39ba7e4b5b6ddd42
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-16 17:07:21 -0700

    How to use mixins from login required for validate user login

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,32 +1,43 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView, CreateView
+from django.forms.utils import ErrorList
+from django import forms
 from .models import Tweet
 from .forms import TweetModelForm
+from .mixins import FormUserNeededMixin
+
 # Create your views here.
 
 # Create
-
-class TweetCreateView(CreateView):
+## Whitout mixins Class declaration
+##class TweetCreateView(CreateView):
+class TweetCreateView(FormUserNeededMixin, CreateView):
     #queryset = Tweet.objects.all()
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = '/tweet/'
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
+    #if whithout mixins class then use this functions (is the same in mixins)
+    # def form_valid(self, form):
+    #     if self.request.user.is_authenticated():
+    #         form.instance.user = self.request.user
+    #         return super(TweetCreateView, self).form_valid(form)
+    #     else:
+    #         form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["el usuario debe estar logueado para continuar"])
+    #         return self.form_invalid(form)
+
 
 # is same the TweetCreateView
-def tweet_create_view(request):
-    form = TweetModelForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.user = request.user
-        instance.save()
-    context = {
-        "form": form
-    }
-    return render(request, 'tweets/create_view.html', context)
+# def tweet_create_view(request):
+#     form = TweetModelForm(request.POST or None)
+#     if form.is_valid():
+#         instance = form.save(commit=False)
+#         instance.user = request.user
+#         instance.save()
+#     context = {
+#         "form": form
+#     }
+#     return render(request, 'tweets/create_view.html', context)
 # Update
 
 # Delete
