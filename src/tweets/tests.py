8f88f0fff8017d45f53a4cd724db14ff9f7491c0
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-20 12:57:33 -0700

    how to use test in django... Sample

--- src/tweets/tests.py
+++ src/tweets/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Tweet
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+
+User = get_user_model()
+
+class TweetModelTestCase(TestCase):
+    def setUp(self):
+        some_random_user = User.objects.create(username='cuahutli68136813');
+
+    def test_tweet_item(self):
+        object = Tweet.objects.create(
+            user= User.objects.first(),
+            content='Some random content'
+        )
+
+        self.assertTrue(object.content, "Some random content")
+        self.assertTrue(object.id, 1)
+        absolute_url = reverse("tweet:detail", kwargs={"pk":1})
+        self.assertTrue(object.get_absolute_url(), absolute_url)
+
+    def test_tweet_url(self):
+        object = Tweet.objects.create(
+            user=User.objects.first(),
+            content='Some random content'
+        )
+
+        absolute_url = reverse("tweet:detail", kwargs={"pk": object.pk})
+        self.assertTrue(object.get_absolute_url(), absolute_url)
