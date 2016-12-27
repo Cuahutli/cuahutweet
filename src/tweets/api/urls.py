d2e172b5ab3798bc4c8f9f1c057e77bf23c7389e
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-27 15:31:30 -0700

    implements a create methos via ajax. and refresh list tweets not reloading page

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.views.generic import RedirectView
 
-from .views import TweetListAPIView
+from .views import (
+    TweetListAPIView,
+    TweetCreateAPIView
+)
 
 urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name='list'), #/api/tweet
+    url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), #/api/tweet
 ]
