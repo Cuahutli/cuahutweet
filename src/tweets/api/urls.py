d05ac7fd2ffb18c0b3c9337f6f64a3e3d7c50561
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-27 13:49:15 -0700

    implement first url api view for ListAPIView.
    my first url for my api

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.views.generic import RedirectView
 
-urlpatterns = [
+from .views import TweetListAPIView
 
+urlpatterns = [
+    url(r'^$', TweetListAPIView.as_view(), name='list'), #/api/tweet
 ]
