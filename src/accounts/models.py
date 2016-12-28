73bf81d48e81e530442cb9cf7c7f9f61e096f913
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-28 14:34:45 -0700

    exclude a following and followers myself

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,6 +3,21 @@ from django.conf import settings
 
 # Create your models here.
 
+class UserProfileManager(models.Manager):
+    use_for_related_fields = True
+
+    def all(self):
+        qs = self.get_queryset().all()
+        #print(dir(self))
+        #print(self.instance) #user
+        ## not auto-following
+        try:
+            if self.instance:
+                qs = qs.exclude(user=self.instance)
+        except:
+            pass
+        return qs
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile') #user.profile
@@ -10,5 +25,11 @@ class UserProfile(models.Model):
     # user.profile.following -- users i follow
     # user.followed_by -- users that follow me -- reverse relationship
 
+    objects = UserProfileManager() # UserProfile.objects.all()
+    #abc = UserProfileManager()  # UserProfile.abc.all()
     def __str__(self):
         return str(self.following.all().count())
+
+    def get_following(self):
+        users = self.following.all() #Users.objects.all().exclude(username=self.user.username)
+        return users.exclude(username=self.user.username)
