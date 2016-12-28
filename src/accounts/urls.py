3635e0b4d3b7fbb3a8478aa2b6cdf67b084186f9
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-12-28 13:26:17 -0700

    add a profile user page

---
+++ src/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.views.generic import RedirectView
+from .views import (
+    UserDetailView
+)
+
+urlpatterns = [
+    url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'), #/user/1/
+]
