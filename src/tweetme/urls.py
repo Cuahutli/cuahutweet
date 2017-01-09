750aa0cb394e75c3d04c9c65ce7432c90400f7fc
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-01-09 14:33:36 -0700

    create a API Tweet User List for show only tweets individual user

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -29,8 +29,8 @@ urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='home'),
     url(r'^tags/(?P<hashtag>.*)/$', HashTagView.as_view(), name = 'hashtag'),
     url(r'^tweet/', include('tweets.urls', namespace='tweet')),
+    url(r'^api/', include('accounts.api.urls', namespace='profiles-api')),
     url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
-    #url(r'^profiles/', include('accounts.urls', namespace='profiles')),
     url(r'^', include('accounts.urls', namespace='profiles')),
 ]
 
