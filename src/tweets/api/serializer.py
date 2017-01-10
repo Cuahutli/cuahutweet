f7849b4c0071c813162c2739dfff02373a6315bf
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-01-10 16:22:03 -0700

    create a reply tweet function

--- src/tweets/api/serializer.py
+++ src/tweets/api/serializer.py
@@ -5,34 +5,11 @@ from tweets.models import Tweet #from ..models import Tweet
 
 
 class ParentTweetModelSerializer(serializers.ModelSerializer):
-    user = UserDisplaySerializer(read_only=True) #write_only
-    date_display = serializers.SerializerMethodField()
-    timesince = serializers.SerializerMethodField()
-    class Meta:
-        model = Tweet
-        fields = [
-            'id',
-            'user',
-            'content',
-            'timestamp',
-            'date_display',
-            'timesince',
-        ]
-
-    def get_date_display(self, obj):
-        return obj.timestamp.strftime("%b %d, %Y a las %I:%M %p")
-
-    def get_timesince(self, obj):
-        return "hace: " + timesince(obj.timestamp)
-
-
-class TweetModelSerializer(serializers.ModelSerializer):
-    user = UserDisplaySerializer(read_only=True) #write_only
-    date_display = serializers.SerializerMethodField()
-    timesince = serializers.SerializerMethodField()
-    parent  = ParentTweetModelSerializer(read_only=True)
-    likes = serializers.SerializerMethodField()
-    did_like = serializers.SerializerMethodField()
+    user            = UserDisplaySerializer(read_only=True) #write_only
+    date_display    = serializers.SerializerMethodField()
+    timesince       = serializers.SerializerMethodField()
+    likes           = serializers.SerializerMethodField()
+    did_like        = serializers.SerializerMethodField()
 
     class Meta:
         model = Tweet
@@ -43,7 +20,6 @@ class TweetModelSerializer(serializers.ModelSerializer):
             'timestamp',
             'date_display',
             'timesince',
-            'parent',
             'likes',
             'did_like',
         ]
@@ -57,16 +33,53 @@ class TweetModelSerializer(serializers.ModelSerializer):
                 return True
         return False
 
-    def get_likes(self, obj):
-        return obj.liked.all().count()
-
     def get_date_display(self, obj):
         return obj.timestamp.strftime("%b %d, %Y a las %I:%M %p")
+
+    def get_likes(self, obj):
+        return obj.liked.all().count()
 
     def get_timesince(self, obj):
         return "hace: " + timesince(obj.timestamp)
 
-    def get_is_retweet(self, obj):
-        if obj.parent:
-            return True
+
+class TweetModelSerializer(serializers.ModelSerializer):
+    user            = UserDisplaySerializer(read_only=True) #write_only
+    date_display    = serializers.SerializerMethodField()
+    timesince       = serializers.SerializerMethodField()
+    parent          = ParentTweetModelSerializer(read_only=True)
+    likes           = serializers.SerializerMethodField()
+    did_like        = serializers.SerializerMethodField()
+
+
+    class Meta:
+        model = Tweet
+        fields = [
+            'id',
+            'user',
+            'content',
+            'timestamp',
+            'date_display',
+            'timesince',
+            'parent',
+            'likes',
+            'did_like',
+            'reply',
+        ]
+
+    def get_did_like(self, obj):
+        request = self.context.get("request")
+        user = request.user
+        if user.is_authenticated():
+            if user in obj.liked.all():
+                return True
         return False
+
+    def get_likes(self, obj):
+        return obj.liked.all().count()
+
+    def get_date_display(self, obj):
+        return obj.timestamp.strftime("%b %d, %Y at %I:%M %p")
+
+    def get_timesince(self, obj):
+        return timesince(obj.timestamp) + " ago"
