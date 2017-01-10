505e9ff8dceda43e4b765bd53f9ba0fbb9775ca1
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-01-10 14:25:22 -0700

    reformat attach method in base.html change a retweet name

--- src/tweets/api/serializer.py
+++ src/tweets/api/serializer.py
@@ -50,12 +50,12 @@ class TweetModelSerializer(serializers.ModelSerializer):
 
     def get_did_like(self, obj):
         request = self.context.get("request")
+        print(request)
         user = request.user
         if user.is_authenticated():
             if user in obj.liked.all():
                 return True
         return False
-
 
     def get_likes(self, obj):
         return obj.liked.all().count()
