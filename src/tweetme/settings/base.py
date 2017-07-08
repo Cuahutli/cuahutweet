87621119a1ab45d6db4380ee7a0ce22460d5ec6d
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-07 19:07:27 -0600

    fix templates

--- src/tweetme/settings/base.py
+++ src/tweetme/settings/base.py
@@ -44,10 +44,11 @@ LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = LOGIN_REDIRECT_URL
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
